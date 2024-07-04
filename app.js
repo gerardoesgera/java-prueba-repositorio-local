2bc828cb0eb5b80d4aebc81444d264b3594b6aea
IngRodriguez <gerardoantoniorodriguezcabrera@gmail.com>
2024-07-03 19:54:44 -0600

    Probando guardar bien en git

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//Variables
 let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
-
+/*
+Este codigo realiza
+la comparacion
+*/
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
 }
